ddcb701b2164f1a1c63e94486b43acd9213647a4
Matt Lyon <matt@flowerpowered.com>
2009-01-29 19:09:41 -0800

    db name templates

--- lib/exegesis.rb
+++ lib/exegesis.rb
@@ -11,12 +11,32 @@ require 'exegesis/design'
 module Exegesis
   
   def self.set_designs_directory dir
-    @design_directory = Pathname.new(dir)
+    @designs_directory = Pathname.new(dir)
+  end
+  
+  def self.designs_directory= dir
+    @designs_directory = Pathname.new(dir)
+  end
+  
+  def self.designs_directory
+    @designs_directory ||= Pathname.new(ENV["PWD"])
+    @designs_directory.to_s
   end
   
   def self.design_file name
-    @design_directory ||= Pathname.new(ENV["PWD"])
-    File.read(@design_directory + name)
+    File.read(designs_directory + name)
+  end
+  
+  def self.database_template= template
+    @db_template = template
+  end
+  
+  def self.database_template
+    @db_template ||= "http://localhost:5984/%s"
+  end
+  
+  def self.database_for name
+    database_template % name
   end
   
 end
