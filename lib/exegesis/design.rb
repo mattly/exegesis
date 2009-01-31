a8406d3c98c026242c7c5518f2e0f4bb29616760
Matt Lyon <matt@flowerpowered.com>
2009-01-31 15:48:49 -0800

    refactoring of design stuff to extract option parsing, and now have docs_for, data_for, keys_for and ids_for

--- lib/exegesis/design.rb
+++ lib/exegesis/design.rb
@@ -24,21 +24,48 @@ module Exegesis
       Exegesis::Document.instantiate database.get(id)
     end
     
-    def docs(view, opts={})
-      if opts.kind_of?(Hash) && opts.has_key?(:starts_with)
-        base = opts.delete(:starts_with)
-        opts.update({:key => base.."#{base}ZZZZZ"})
-      elsif ! opts.kind_of?(Hash) || ([:key, :keys, :startkey, :endkey] & opts.keys).empty?
-        opts = {:key => opts}
+    def parse_opts(opts={})
+      if opts[:key]
+        case opts[:key]
+        when Range
+          range = opts.delete(:key)
+          opts.update({:startkey => range.first, :endkey => range.last})
+        when Array
+          if opts[:key].any?{|v| v.kind_of?(Range) }
+            key = opts.delete(:key)
+            opts[:startkey] = key.map {|v| v.kind_of?(Range) ? v.first : v }
+            opts[:endkey]   = key.map {|v| v.kind_of?(Range) ? v.last : v }
+          end
+        end
       end
-      
-      if opts[:key].is_a?(Range)
-        range = opts.delete(:key)
-        opts.update({:startkey => range.first, :endkey => range.last})
-      end
-      
-      response = database.view "#{design_doc}/#{view}", opts.update({:include_docs => true})
+
+      opts
+    end
+    
+    def view view_name, opts={}
+      opts = parse_opts opts
+      database.view "#{design_doc}/#{view_name}", opts
+    end
+    
+    def docs_for view_name, opts={}
+      response = view view_name, opts.update({:include_docs => true})
       response['rows'].map {|doc| Exegesis::Document.instantiate doc['doc'] }
     end
+    
+    def data_for view_name, opts={}
+      response = view view_name, opts
+      response['rows'].map {|row| row['value'] }
+    end
+    
+    def keys_for view_name, opts={}
+      response = view view_name, opts
+      response['rows'].map {|row| row['key'] }
+    end
+    
+    def ids_for view_name, opts={}
+      response = view view_name, opts
+      response['rows'].map {|row| row['id'] }
+    end
+    
   end
 end
--- test/design_test.rb
+++ test/design_test.rb
@@ -33,86 +33,63 @@ class ExegesisDesignTest < Test::Unit::TestCase
       reset_db('design-views')
       @doc = Foos.new(@db)
       @raw_docs = [
-        {'_id' => 'foo', 'foo' => 'bar', 'bar' => 'bee', '.kind' => 'TestForDesign'},
-        {'_id' => 'bar', 'foo' => 'baz', 'bar' => 'bee', '.kind' => 'TestForDesign'},
-        {'_id' => 'baz', 'foo' => 'foo', 'bar' => 'bee', '.kind' => 'TestForDesign'}
+        {'_id' => 'foo', 'foo' => 'foo', 'bar' => 'foo', '.kind' => 'TestForDesign'},
+        {'_id' => 'bar', 'foo' => 'bar', 'bar' => 'bar', '.kind' => 'TestForDesign'},
+        {'_id' => 'baz', 'foo' => 'baz', 'bar' => 'baz', '.kind' => 'TestForDesign'}
       ]
       @db.bulk_save @raw_docs
       @db.save({
         '_id' => '_design/foos',
         'views' => {
-          'string' => { 'map'=>'function(doc) {emit(doc.foo, doc.bar);}' },
-          'array' => {'map' => 'function(doc) {emit([doc.bar, doc.foo], doc.bar)}'},
-          'hash' => {'map' => 'function(doc) {emit({foo: doc.foo}, doc.bar)}'}
+          'test' => { 'map'=>'function(doc) {emit(doc.foo, doc.bar);}' },
         }
       })
     end
     
+    context "parsing options" do
+      context "when the key is a range" do
+        before { @opts = @doc.parse_opts(:key => 'bar'..'baz') }
+        
+        expect { @opts[:key].will == nil }
+        expect { @opts[:startkey].will == 'bar' }
+        expect { @opts[:endkey].will == 'baz' }
+      end
+      
+      context "when the key is an array with a range in it" do
+        before { @opts = @doc.parse_opts(:key => ['published', '2008'..'2008/13']) }
+        
+        expect { @opts[:key].will be(nil) }
+        expect { @opts[:startkey].will == ['published', '2008'] }
+        expect { @opts[:endkey].will == ['published', '2008/13'] }
+      end
+    end
+    
     context "with docs" do
-      context "for a simple key" do
-        before do
-          @response = @doc.docs(:string, 'foo')
-        end
-      
-        expect { @response.will be_kind_of(Array) }
-        expect { @response.size.will == 1 }
-        expect { @response.first.will be_kind_of(TestForDesign) }
-        expect { @response.first['foo'].will == 'foo' }
-      end
-      
-      context "for an array key" do
-        before do
-          @response = @doc.docs(:array, ['bee','foo'])
-        end
-      
-        expect { @response.will be_kind_of(Array) }
-        expect { @response.size.will == 1 }
-        expect { @response.first.will be_kind_of(TestForDesign) }
-        expect { @response.first['foo'].will == 'foo'}
-      end
-      
-      context "for a hash key" do
-        before do
-          @response = @doc.docs(:hash, {'foo' => 'foo'})
-        end
-      
-        expect { @response.will be_kind_of(Array) }
-        expect { @response.size.will == 1 }
-        expect { @response.first.will be_kind_of(TestForDesign) }
-        expect { @response.first['foo'].will == 'foo' }
-      end
-
-      context "for a range" do
-        before do
-          @response = @doc.docs(:string, 'bar'..'baz')
-        end
-        
-        expect { @response.will be_kind_of(Array) }
-        expect { @response.size.will == 2 }
-        expect { @response.each {|d| d.will be_kind_of(TestForDesign) }}
-        expect { @response.first['foo'].will == 'bar' }
-        expect { @response.last['foo'].will == 'baz' }
-      end
-      
-      context "for a range with :starts_with" do
-        before do
-          @response = @doc.docs(:string, :starts_with => 'ba')
-        end
-        
-        expect { @response.will be_kind_of(Array) }
-        expect { @response.size.will == 2 }
-        expect { @response.first.will be_kind_of(TestForDesign) }
-        expect { @response.first['foo'].will == 'bar' }
-        expect { @response.last['foo'].will == 'baz' }
-      end
-      
+      before { @response = @doc.docs_for :test, :key => 'foo' }
+    
+      expect { @response.will be_kind_of(Array) }
+      expect { @response.size.will == 1 }
+      expect { @response.first.will be_kind_of(TestForDesign) }
+      expect { @response.first['foo'].will == 'foo' }
     end
     
     context "for the view's data" do
-      before do
-        # @docs = 
-      end
-    end 
+      before { @response = @doc.data_for :test }
+      
+      expect { @response.will == %w(bar baz foo) }
+    end
+    
+    context "for the view's matching keys" do
+      before { @response = @doc.keys_for :test, :key => 'bar'..'baz' }
+      
+      expect { @response.will == %w(bar baz) }
+    end
+    
+    context "for the view's matching ids" do
+      before { @response = @doc.ids_for :test, :key => 'bar'..'foo'}
+      
+      expect { @response.will == %w(bar baz foo) }
+    end
   end
   
 end
